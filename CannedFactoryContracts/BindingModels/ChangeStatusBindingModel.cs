1aa8e491ae75872e4e43d3e4f01498fc3a11a49a
Alina5757 <91009972+Alina5757@users.noreply.github.com>
2022-05-14 16:46:41 +0400

    Laba 6

--- CannedFactoryContracts/BindingModels/ChangeStatusBindingModel.cs
+++ CannedFactoryContracts/BindingModels/ChangeStatusBindingModel.cs
@@ -8,5 +8,6 @@ namespace CannedFactoryContracts.BindingModels
     public class ChangeStatusBindingModel
     {
         public int OrderId { get; set; }
+        public int? ImplementerId { get; set; }
     }
 }
